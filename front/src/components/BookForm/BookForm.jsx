4f482612ff9452e7b27410f9ea5587d2e8169b21
Dmitry Kuchukov <kudim@inbox.ru>
2024-07-16 19:23:31 +0300

    refactor: add separate function

--- front/src/components/BookForm/BookForm.jsx
+++ front/src/components/BookForm/BookForm.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../../redux/books/actionCreators'
 import booksData from '../../data/books.json'
 import './BookForm.css'
+import createBookWithID from '../../utils/createBookWithID';
 
 export default function BookForm() {
     const [title, setTitle] = useState('')
@@ -15,12 +15,7 @@ export default function BookForm() {
         e.preventDefault()
         if (title && author) {
 
-            const book = {
-                title,
-                author,
-                id: uuidv4(),
-                isFavorite: false
-            }
+            const book = createBookWithID({ title, author })
 
             dispatch(addBook(book))
             setTitle('')
@@ -31,13 +26,7 @@ export default function BookForm() {
     const handleAddRandomBook = () => {
         const randomIndex = Math.floor(Math.random() * booksData.length)
         const randomBook = booksData[randomIndex]
-
-        const randomBookWithId = {
-            ...randomBook,
-            id: uuidv4(),
-            isFavorite: false
-        }
-
+        const randomBookWithId = createBookWithID(randomBook)
         dispatch(addBook(randomBookWithId))
     }
 
