d8de87cf9d3d6cdeac87f8fa20e5a7357f942eb1
Dmitry Kuchukov <kudim@inbox.ru>
2024-07-18 06:55:23 +0300

    feat: add onChange to Filter

--- front/src/components/Filter/Filter.jsx
+++ front/src/components/Filter/Filter.jsx
@@ -1,9 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { setTitleFilter, selectTitleFilter } from '../../redux/slices/filterSlice'
 import './Filter.css'
 
 export default function Filter() {
+    const dispatch = useDispatch()
+    const titleFilter = useSelector(selectTitleFilter)
+
+    const handleTitleFilterChange = (e) => {
+        dispatch(setTitleFilter(e.target.value))
+    }
     return (
         <div className='app-block filter'>
-            <h2>Filter</h2>
+            <div className="filter-group">
+                <input type="text" placeholder='Filter by title...' onChange={handleTitleFilterChange} value={titleFilter} />
+            </div>
+
         </div>
     )
 }
