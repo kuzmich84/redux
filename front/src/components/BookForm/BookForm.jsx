41d22696c53658cf726a55edddd4cf1faf426e6d
Dmitry Kuchukov <kudim@inbox.ru>
2024-07-20 16:51:20 +0300

    feat: move isLoading to booksSlice

--- front/src/components/BookForm/BookForm.jsx
+++ front/src/components/BookForm/BookForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { addBook, fetchBook } from '../../redux/slices/booksSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { addBook, fetchBook, isLoadingViaAPI } from '../../redux/slices/booksSlice'
 import booksData from '../../data/books.json'
 import './BookForm.css'
 import createBookWithID from '../../utils/createBookWithID'
@@ -11,8 +11,9 @@ import { FaSpinner } from "react-icons/fa";
 export default function BookForm() {
     const [title, setTitle] = useState('')
     const [author, setAuthor] = useState('')
-    const [isLoading, setIsLoading] = useState(false)
+
     const dispatch = useDispatch()
+    const isLoading = useSelector(isLoadingViaAPI)
 
 
     const handleSubmit = (e) => {
@@ -37,16 +38,7 @@ export default function BookForm() {
     }
 
     const handleAddRandomBookViaAPI = async () => {
-        try {
-            setIsLoading(true)
-            await dispatch(fetchBook('http://localhost:4000/random-book-delayed'));
-        } catch (error) {
-            setIsLoading(false)
-        } finally {
-            setIsLoading(false)
-        }
-
-
+        dispatch(fetchBook('http://localhost:4000/random-book-delayed'));
     }
 
 
